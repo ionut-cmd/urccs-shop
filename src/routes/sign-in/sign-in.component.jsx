8449045cdc27617da18e6199cea021ce2db2e9e6
Ionut <bostanionut28@gmail.com>
2022-08-07 11:41:57 +0100

    setup firebase and user authentification with Google

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,20 @@
+import { 
+    signInWithGooglePopup,
+    createUserDocumentFromAuth 
+} from "../../utils/firebase/fitebase.utils";
+
 const SignIn = () => {
+    const logGoogleUser = async () => {
+        const {user} = await signInWithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user); 
+        console.log(user);
+    }
     return (
         <div>
             <h1>Sign in page</h1>
+            <button onClick={logGoogleUser}>
+                sign in with Google popup
+            </button>
         </div>
     )
 }
