ea2844b00286c8e8ed8cf54098c5c83c0fb3d799
Ionut <bostanionut28@gmail.com>
2022-08-29 13:37:28 +0100

    added checkout

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,10 +1,42 @@
-
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
+import ChechoutItem from '../../components/checkout-item/checkout-item.component';
+import './checkout.styles.scss';
 
 
 const Checkout = () => {
+
+    const  { cartItems,cartTotal } = useContext(CartContext);
     return(
-        <div>
-            <h1>Whelcome to checkout</h1>
-        </div>           
-    )
+        <div className="checkout-container">
+            <div className="checkout-header">
+                <div className="header-block">
+                    <span>Product</span>
+                </div>
+                <div className="header-block">
+                    <span>Description</span>
+                </div>
+                <div className="header-block">
+                    <span>Quantity</span>
+                </div>
+                <div className="header-block">
+                    <span>Price</span>
+                </div>
+                <div className="header-block">
+                    <span>Remove</span>
+                </div>
+            </div>
+            
+            {
+                cartItems.map((cartItem) => {
+                    return (
+                        <ChechoutItem key={cartItem.id} cartItem = {cartItem}/>
+                )})
+            }
+                <span className="total">Total: ${cartTotal}</span>
+            </div>    
+    ) 
+
 }
+
+export default Checkout;
